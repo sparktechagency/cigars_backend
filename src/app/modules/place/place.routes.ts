6d62b8f9e4e5c2c8946df2f765fd7f7e13def7cf
manik <maniksarker652@gmail.com>
2025-03-08 14:59:49 +0600

    route setup for places

---
+++ src/app/modules/place/place.routes.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import validateRequest from '../../middlewares/validateRequest';
+import PlaceValidations from './place.validation';
+import PlaceController from './place.controller';
+
+const router = express.Router();
+
+router.post(
+  '/add-place',
+  auth(USER_ROLE.user, USER_ROLE.superAdmin),
+  validateRequest(PlaceValidations.createPlaceValidationSchema),
+  PlaceController.addPlace,
+);
+router.get(
+  '/get-all-place',
+  auth(USER_ROLE.user, USER_ROLE.superAdmin),
+  PlaceController.getAllPlace,
+);
+router.get(
+  '/get-single-place/:id',
+  auth(USER_ROLE.user, USER_ROLE.superAdmin),
+  PlaceController.getSinglePlace,
+);
+
+export const placeRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,6 +5,7 @@ import { ManageRoutes } from '../modules/manage-web/manage.routes';
 import { normalUserRoutes } from '../modules/normalUser/normalUser.routes';
 
 import { notificationRoutes } from '../modules/notification/notification.routes';
+import { placeRoutes } from '../modules/place/place.routes';
 
 const router = Router();
 
@@ -29,6 +30,10 @@ const moduleRoutes = [
   {
     path: '/notification',
     router: notificationRoutes,
+  },
+  {
+    path: '/place',
+    router: placeRoutes,
   },
 ];
 
