25e50ed5e4253f464b76fa660af605101574b880
manik <maniksarker652@gmail.com>
2025-03-09 08:35:16 +0600

    create service for regulation

--- src/app/modules/regulation/regulation.service.ts
+++ src/app/modules/regulation/regulation.service.ts
@@ -1,3 +1,5 @@
+import httpStatus from 'http-status';
+import AppError from '../../error/appError';
 import { IRegulation } from './regulation.interface';
 import Regulation from './regulation.model';
 
@@ -6,8 +8,22 @@ const createRegulation = async (payload: IRegulation) => {
   return result;
 };
 
+const updateRegulation = async (id: string, payload: Partial<IRegulation>) => {
+  const regulation = await Regulation.findById(id);
+  if (!regulation) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Regulation not found');
+  }
+
+  const result = await Regulation.findByIdAndUpdate(id, payload, {
+    new: true,
+    runValidators: true,
+  });
+  return result;
+};
+
 const RegulationService = {
   createRegulation,
+  updateRegulation,
 };
 
 export default RegulationService;
