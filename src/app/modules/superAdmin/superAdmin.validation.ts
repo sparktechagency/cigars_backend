7404ad61189c72a6f7fecf280cf2ba8d6b93445d
manik <maniksarker652@gmail.com>
2025-03-10 09:09:29 +0600

    update super sadmin validation shcema

--- src/app/modules/superAdmin/superAdmin.validation.ts
+++ src/app/modules/superAdmin/superAdmin.validation.ts
@@ -1,15 +1,13 @@
 import { z } from 'zod';
 
 export const updateSuperAdminSchema = z.object({
-  name: z.string().nonempty('Name is required').optional(),
-  username: z.string().nonempty('Username is required').optional(),
-  phone: z.string().optional(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
   email: z
     .string()
     .email('Invalid email format')
     .nonempty('Email is required')
     .optional(),
-  address: z.string().optional(),
   profile_image: z.string().optional().default(''),
 });
 
