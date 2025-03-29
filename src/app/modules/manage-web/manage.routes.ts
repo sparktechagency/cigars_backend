3d42238a9adc5a6415496fcd6ba06345ba8824d2
manik <maniksarker652@gmail.com>
2025-03-29 16:55:48 +0600

    make some changes

--- src/app/modules/manage-web/manage.routes.ts
+++ src/app/modules/manage-web/manage.routes.ts
@@ -38,37 +38,13 @@ router.post(
   uploadFile(),
   ManageController.addSlider,
 );
-router.get(
-  '/get-privacy-policy',
-  // auth(USER_ROLE.superAdmin),
-  ManageController.getPrivacyPolicy,
-);
-router.get(
-  '/get-partner',
-  // auth(USER_ROLE.superAdmin),
-  ManageController.getPartner,
-);
-router.get(
-  '/get-slider',
-  // auth(USER_ROLE.superAdmin),
-  ManageController.getSlider,
-);
+router.get('/get-privacy-policy', ManageController.getPrivacyPolicy);
+router.get('/get-partner', ManageController.getPartner);
+router.get('/get-slider', ManageController.getSlider);
 router.get('/get-faq', ManageController.getFAQ);
-router.get(
-  '/get-about-us',
-  // auth(USER_ROLE.superAdmin),
-  ManageController.getAboutUs,
-);
-router.get(
-  '/get-terms-conditions',
-  // auth(USER_ROLE.superAdmin),
-  ManageController.getTermsConditions,
-);
-router.get(
-  '/get-contact-us',
-  // auth(USER_ROLE.superAdmin),
-  ManageController.getContactUs,
-);
+router.get('/get-about-us', ManageController.getAboutUs);
+router.get('/get-terms-conditions', ManageController.getTermsConditions);
+router.get('/get-contact-us', ManageController.getContactUs);
 router.patch(
   '/edit-privacy-policy/:id',
   auth(USER_ROLE.superAdmin),
